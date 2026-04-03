e5f815b8872a66f3b311691b7478cd826a2e9294
Aquib <aquib.shaikh256@gmail.com>
2026-04-03 12:27:22 +0530

    Revised and formated in proper structure

--- Java_1D_Arrays/Array1.java
+++ Java_1D_Arrays/Array1.java
@@ -4,39 +4,38 @@
 
 
 //  //***********************Calculating sum by passing array in funtion*************************
-//   // public static void sum(int[] arr){
-//   //   int sum = 0;
-//   // //     for(int i = 0; i < 4; i++){
-//   // //       sum+= arr[i]; //Another loop that goes through each element in the array and adds them to sum.
-//   // //     }
-//   // }
-//   // // public static void main(String[] args) {
-//   // //     Scanner scn = new Scanner(System.in);
-//   // //     int[] arr = new int[4];
-//   // //     for(int i=0;i<4;i++){ //instead of 4 we can take arr.length to take n number of inputs from user
-//   // //       arr[i]=scn.nextInt(); //This loop asks the user to input 4 numbers, which are stored in the array.
-//   // //     }
-//   // //    
-//   // //     System.out.println(sum);
-//   // // }
+  public static void sum(int[] arr){
+    int sum = 0;
+  //     for(int i = 0; i < 4; i++){
+  //       sum+= arr[i]; //Another loop that goes through each element in the array and adds them to sum.
+  //     }
+  }
+  // public static void main(String[] args) {
+  //     Scanner scn = new Scanner(System.in);
+  //     int[] arr = new int[4];
+  //     for(int i=0;i<4;i++){ //instead of 4 we can take arr.length to take n number of inputs from user
+  //       arr[i]=scn.nextInt(); //This loop asks the user to input 4 numbers, which are stored in the array.
+  //     }
+  //    
+  //     System.out.println(sum);
+  // }
 
 
 // //******************Printing Array******************************************************************
-//   //  static void printArray(int[] arr){
-//   //   int n = arr.length; //size of array 
-//   //    for(int i =0;i<n;i++){
-//   //       System.out.println(arr[i]);
-//   //     }
-//   //  }
+  //  static void printArray(int[] arr){
+  //   int n = arr.length; //size of array 
+  //    for(int i =0;i<n;i++){
+  //       System.out.println(arr[i]);
+  //     }
+  //  }
 
-//   // public static void main(String[] args){
-//   //   Scanner scn = new Scanner(System.in);
-//   //     int[] arr = new int[4];
-//   //     for(int i=0;i<4;i++){
-//   //       arr[i]=scn.nextInt();
-//   //     }
-//   //     printArray(arr);
-// }
+  // public static void main(String[] args){
+  //   Scanner scn = new Scanner(System.in);
+  //     int[] arr = new int[4];
+  //     for(int i=0;i<4;i++){
+  //       arr[i]=scn.nextInt();
+  //     }
+  //     printArray(arr);
 
 // *******************Increasing each value by 1**********************************************************
 
@@ -68,28 +67,28 @@
 
 // **********************************************in increasing order from 1 to n*************************************
  
-//     //arr --> 0,1,2.3,4.... n-1
-//     // we want output like this--> 1,2,3,4,...n
-//      public static void main(String[] args){
-//     //Write a code to initialize an array
-//     //in increasing order from 1 to n
+    //arr --> 0,1,2,3,4.... n-1
+    // we want output like this--> 1,2,3,4,...n
+     public static void main(String[] args){
+    //Write a code to initialize an array
+    //in increasing order from 1 to n
    
-//    //checking input 
-//     Scanner scn = new Scanner(System.in);
-//     int n = scn.nextInt();
+   //checking input 
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
 
-//     int arr[] = new int[n];
-//     //arr --> 0,1,2,3,4.... n-1
-//     // we want output like this--> 1,2,3,4,...n
-//     for(int i=0;i<n;i++){
-//             arr[i] = i+1;
-//           }
-//    //check and print the array
-//    for(int i = 0;i < n;i++){
-//      System.out.println(arr[i]);
-//    }
-//   }
-// }
+    int arr[] = new int[n];
+    //arr --> 0,1,2,3,4.... n-1
+    // we want output like this--> 1,2,3,4,...n
+    for(int i=0;i<n;i++){
+            arr[i] = i+1;
+          }
+   //check and print the array
+   for(int i = 0;i < n;i++){
+     System.out.println(arr[i]);
+   }
+  }
+}
 
 
 //*********************************************************Assignment problem************************************************
