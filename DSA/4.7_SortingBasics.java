9d87de9f0ba6eea57f7c400326cb0e6cd9eedd79
Aquib <aquib.shaikh256@gmail.com>
2026-05-05 11:26:18 +0530

    Revised and solved few pbs on sorting

--- DSA/4.7_SortingBasics.java
+++ DSA/4.7_SortingBasics.java
@@ -13,29 +13,40 @@ return cost;
 
 //Given an array of distinct elements find the count of noble integers
  
-int findNobelInt(int[] mat){
-  Arrays.sort(mat); //TC : O(NlogN)
-  int ans = 0 ;
-  for(int i = 0 ; i < n ; i++){
-    if(arr[i] == i){
-      ans++;
-    }
-  }
-} 
+import java.util.*;
 
-//Given an array count the nobleInteger of it.
-import java.util.Arrays;
 public class Main {
-  static int nobleInteger(int[] A){
-   Arrays.sort(A);
+  static int nobleElem(int[] arr){
+    Arrays.sort(arr);
+    
+    int n = arr.length;
+    int ans = 0;
+    for(int i = 0 ; i < n ; i++){
+      if(arr[i] == i){
+        ans++;
+      }
+    }
+    return ans;
+  }
+  public static void main(String[] args) {
+     int[] arr = {-3,0,2,5};
+
+     System.out.print(nobleElem(arr));
+  }
+}
+//Given an array count the nobleInteger of it.
+import java.util.Arrays; //important for arrays
+public class Main {
+  static int nobleIntegerIndistinct(int[] A){
+   Arrays.sort(A); //NlogN
    int n = A.length;
    int ans = 0 ;
    int count_less_elem = 0 ;
 
-   if(A[0] == 0){
+   if(A[0] == 0){ //edge case
     ans++;
    }
-   for(int i = 1 ; i < n ; i++){
+   for(int i = 1 ; i < n ; i++){ //don't forget to start from 1 since we already take care of edge case
     if(A[i] != A[i-1]){
       count_less_elem = i;
     }
@@ -47,7 +58,7 @@ public class Main {
 }
   public static void main(String[] args) {
   int[] A = {0,2,2,3,3,6};
-  System.out.print(nobleInteger(A));
+  System.out.print(nobleIntegerIndistinct(A));
      
   }
 }
@@ -55,21 +66,31 @@ public class Main {
 
 //Selection sorting -->
 
-void SelectionSort(int[] arr){
-  int n = arr.length;
+import java.util.*;
 
-  for(int i = 0 ; i < n ; i++){        // Step 1: go through each index
-    int minIndex = i;                  // assume the current index has the smallest value
-    for(int j = i+1 ; j < n ; j++){    // Step 2: check the rest of the array
-      if(arr[j] < arr[minIndex]){      // if we find something smaller
-        minIndex = j;                  // update minIndex
+public class Main {
+  static void selectionSort(int[] arr){
+    int n = arr.length;
+    
+    for(int i = 0 ; i < n ; i++){
+      int minIndex = i;
+
+      for(int j = i+1 ; j < n ; j++){
+        if(arr[j] < arr[minIndex]){
+          minIndex = j;
+        }
       }
+      int temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
     }
+  }
+  public static void main(String[] args) {
+     int[] arr = {1,4,2,5,3};
+     
+     selectionSort(arr);
+     System.out.print(Arrays.toString(arr));
 
-    // Step 3: swap the smallest found element with arr[i]
-    int temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
   }
 }
 
@@ -101,3 +122,5 @@ public class Main {
     System.out.println(solve(A));  // Output should be 1
   }
 }
+
+
