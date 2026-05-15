4a3660d30fc96c068d68646ffd61b2f51c721790
Aquib <aquib.shaikh256@gmail.com>
2026-05-15 16:21:51 +0530

    Solved multiple problems around pattern

--- Java_Loops/pattern2.java
+++ Java_Loops/pattern2.java
@@ -232,3 +232,28 @@ public class Main {
       }
   }
 }
+
+//reverse full pyramid
+
+* * * * *
+  * * * *
+   * * *
+    * *
+     *
+     
+public class Main {
+  public static void main(String[] args) {
+     int n = 5;
+     
+     for(int i = 1 ; i <= n ; i++){
+      for(int j = 1; j < i ; j++){
+        System.out.print(" ");
+      }
+      for(int j = 1 ; j <= n+1 - i ; j++){
+        System.out.print(" *");
+      }
+
+      System.out.println();
+     }
+  }
+}
