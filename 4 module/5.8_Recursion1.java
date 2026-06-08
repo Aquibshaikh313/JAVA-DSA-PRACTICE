6480b6565640f5f05363b6b17be0b73f7a0dee7e
Aquib <aquib.shaikh256@gmail.com>
2026-06-08 19:55:57 +0530

    Solved few problems on Recursion

--- 4 module/5.8_Recursion1.java
+++ 4 module/5.8_Recursion1.java
@@ -65,3 +65,17 @@ class Solution {
         return false;
     }
 }
+
+//fibonacii 0 1 1 2 3 5 8 13
+
+class Solution {
+  public static void fibonacii(int n){
+    if(n==0 || n == 1){
+      return n;
+    }
+    int a = fib(n-1);
+    int b = fib(n-2);
+    return a + b;
+  }
+}
+// if n = 4 o/p = 3
