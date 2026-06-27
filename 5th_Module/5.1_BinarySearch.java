9bb3d8959061a6cc30026b9733c3cbcb1764eb6e
Aquib <aquib.shaikh256@gmail.com>
2026-06-27 14:06:15 +0530

    Completed binary search lec 1

--- 5th_Module/5.1_BinarySearch.java
+++ 5th_Module/5.1_BinarySearch.java
@@ -1,4 +1,7 @@
 
+
+
+//finding the target element
 public class Main {
   static int findTarget(int[] nums, int target){
   int n = nums.length;
@@ -25,5 +28,151 @@ public class Main {
 
       System.out.print(findTarget(nums,target));
 
+  }
+}
+
+//Finding first occurence of the element:
+
+public class Main {
+  static int findFirstOcc(int[] arr,int k){
+    int n = arr.length;
+    int low = 0;
+    int hi = n-1;
+    int ans = -1;
+
+    while(low <= hi){
+      int m = (low + hi)/2;
+
+      if(arr[m] == k){
+        ans = m;
+        hi = m-1;
+        
+      }
+      else if(arr[m] > k){
+        hi = m-1;
+      }else{
+        low = m+1;
+      }
+    }
+    return ans;
+  }
+  public static void main(String[] args) {
+     int[] arr = {-5,0,0,1,1,2,3,3,5,5,5,5,5,5,9};
+     int k = 5;
+
+     System.out.print(findFirstOcc(arr,k));
+
+  }
+}
+
+
+//finding last occurence of an element
+
+public class Main {
+  static int lastOccurence(int[] arr,int k){
+    int n = arr.length;
+    int low = 0; 
+    int hi = n - 1;
+    int ans = -1;
+
+    while(low <= hi){
+      int m = (low+hi)/2;
+      if(arr[m] == k){
+        ans = m;
+        low = m +1;
+      }
+      else if(arr[m] > k){
+        hi = m - 1;
+      }else{
+        low = m+1;
+      }
+    }
+    return ans;
+  }
+  public static void main(String[] args) {
+      int[] arr = {-5,0,0,1,1,2,3,3,5,5,5,5,5,5,9};
+     int k = 5;
+
+     System.out.print(lastOccurence(arr,k));
+
+  }
+}
+
+//finding local minima
+//finding local minima
+public class Main {
+  static int localMinima(int[] arr){
+    int n = arr.length;
+   
+    if(n==1){
+      return arr[0];
+    }
+    if(arr[0] < arr[1]){
+     return arr[0];
+    }
+    if(arr[n-1] < arr[n-2]){
+     return arr[n-1];
+    }
+   int low = 1; 
+   int hi = n-2;
+    while(low <= hi){
+      int m = (low+hi)/2;
+      if(arr[m] < arr[m-1] && arr[m] < arr[m+1]){
+        return arr[m];
+      }
+      else if(arr[m] < arr[m-1] && arr[m] > arr[m+1]){
+        low= m+1;
+        
+      }
+      else{
+        hi = m-1;
+      }
+    }
+
+    return -1;
+    
+  }
+  public static void main(String[] args) {
+     int[] arr = {12, -4, 7, 0, 19, 5, -1, 8, 3, 15, 2, 9, 6, 14, 1};
+
+     System.out.print(localMinima(arr));
+  }
+}
+
+
+//finding the uniq elements in given arr
+
+
+public class Main {
+  static int findUniqElem(int[] arr){
+    int n = arr.length;
+    if(n == 1) return arr[0];
+    if(arr[0] != arr[1]) return arr[0];
+    if(arr[n-1] != arr[n-2]) return arr[n-1];
+
+    int low = 1;
+    int hi = n-2;
+  
+
+    while(low <= hi){
+      int m = (low+hi)/2;
+      if(arr[m] != arr[m-1]&& arr[m]!= arr[m+1]){
+        return arr[m];
+      }
+      if(arr[m] == arr[m-1]){
+        m = m - 1;
+      }
+      if(m % 2 == 0 ){
+        low = m + 2 ;
+      }else{
+        hi = m - 1;
+      }
+    }
+    return -1;
+  }
+  public static void main(String[] args) {
+      int[] arr = {3,3,2,2,1,1,5,5,7,7,9,0,0};
+
+      System.out.print(findUniqElem(arr));
   }
 }
